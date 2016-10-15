792aacf839b5f2d5475b4009d970fa89bb4f423e
Danya Generalov <danya02@me.com>
2016-10-15 19:58:32 +0300

    Adding program to add keybook entries.

---
+++ keybook_mantainer/simple.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import json
+
+
+def add_object(diction):
+    import os
+    try:
+        obj = json.load(open(
+            os.path.expanduser("~/.rsa-communicator/keybook.json")))
+    except:
+        obj = []
+    obj += [diction]
+    json.dump(obj, open(
+        os.path.expanduser("~/.rsa-communicator/keybook.json")))
+if __name__ == '__main__':
+    print("--- RSA keybook mantainer ---")
+    print("Adding a keybook entry...")
+    correct = False
+    obj = {}
+    while not correct:
+        obj["name"] = input("Name: ")
+        keypath = input("Path to key file: ")
+        import os
+        os.makedirs(os.path.expanduser("~/.rsa-communicator/keys"))
+        key = open(os.path.abspath(os.path.expanduser(keypath)), "b").read()
+        import uuid
+        keysave = open(os.path.expanduser(
+            "~/.rsa-communicator/keys/"+uuid.uuid4()), "wb")
+        keysave.write(key)
+        keysave.close()
+        obj["path"] = keysave.name
+        if input("Is this information correct? (y/N) ").capitalize() == "Y":
+            correct = True
+    add_object(obj)
